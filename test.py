a5d613e16fdafe7f4862c158265c3c76a825b29a
AYOUB BOULBAZ <boulbaz147@gmail.com>
2022-06-01 03:31:01 +0100

    Upload avatar is done, now moving to the frontend

--- test.py
+++
@@ -1,9 +0,0 @@
-def reverse(str):
-    resule = ""
-    for x in range(0, len(str)):
-        resule = resule + str[len(str) - 1 - x]
-    return (resule)
-print("Give me the word:")
-x = input()
-print(reverse(x))
-
