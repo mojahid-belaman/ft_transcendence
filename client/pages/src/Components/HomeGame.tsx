aa620ddc64b84f6e8cb87cdc2f9a5884d8217f54
AYOUB BOULBAZ <boulbaz147@gmail.com>
2022-07-24 17:11:02 +0100

    Adding socket

---
+++ client/pages/src/Components/HomeGame.tsx
@@ -0,0 +1,68 @@
+import { useContext, useEffect, useState } from "react";
+import styles from "../../../styles/HomeGame.module.css";
+import Game from "./Game";
+import Cookies from "js-cookie";
+import socket from "../../../Library/Socket";
+// import SocketContext from "../main/navigationbar/data_context/socket-context";
+
+export function HomeGame() {
+
+  const [isGame, setIsGame] = useState(false);
+
+  const gameDefHandler = () => {
+    const token = Cookies.get("access_token");
+    socket.emit("join_match", {
+      access_token: token,
+      type: "default",
+    });
+    setIsGame(true);
+  };
+  
+  const gameObsHandler = () => {
+    const token = Cookies.get("access_token");
+    socket.emit("join_match", {
+      access_token: token,
+      type: "obstacle",
+    });
+    setIsGame(true);
+  };
+
+  return (
+    <>
+        {
+          !isGame ? (<div className={styles.container}>
+            <div className={styles.game}>
+              <img src="/pingpong.png" alt="Ping Pong Game" />
+            </div>
+            <div className={styles.about}>
+              <h1>
+                Ping
+                <img src="/racket.png" alt="racket" width="80px" height="60px" />
+                Pong
+              </h1>
+              <p>
+                <span className={styles.title}>PING PONG</span> is a table tennis
+                game where you can enjoy a real match experience.
+                <br />
+                You can enjoy the feeling of an actual table tennis by tossing and
+                serving the ball, and hitting back to a different direction by
+                adjusting the angle of the racket.
+                <br />
+                You can discorver it by yourself &nbsp;
+                <span className={styles.emoji}>😉</span>
+              </p>
+              <div className={styles.centerbtn}>
+                <button className={styles.btnDef} onClick={gameDefHandler}>
+                  MEDIUM
+                </button>
+                <button className={styles.btnObs} onClick={gameObsHandler}>
+                  HARD
+                </button>
+              </div>
+            </div>
+          </div>) : <Game />
+        }
+        
+    </>
+  );
+}
