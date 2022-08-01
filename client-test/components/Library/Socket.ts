6feaeb52c6042a4ad033a5579d6b6f25b171e610
AYOUB BOULBAZ <boulbaz147@gmail.com>
2022-08-01 22:10:27 +0100

    Adding undefined to query's token

--- client-test/components/Library/Socket.ts
+++ client-test/components/Library/Socket.ts
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 const token = Cookies.get("access_token")
 
 const socket = io("http://localhost:5000", {
-    query: {token: token ? token : ""}
+    query: {token: token ? token : undefined}
 });
 
 export default socket;
