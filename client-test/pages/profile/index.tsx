3067b40e569baee06b760781b212f1bc0b8042d1
mojahid-belaman <mojahidbelaman@gmail.com>
2022-08-02 14:27:02 +0100

    update style components

--- client-test/pages/profile/index.tsx
+++ client-test/pages/profile/index.tsx
@@ -8,28 +8,28 @@ import Profile from "../../components/profileComponents/Profile";
 
 function chat() {
 
-        const history = useRouter();
+    //     const history = useRouter();
 
-	const authHandler = async () => {
-		const token = Cookies.get("access_token")
-		if (token)
-			await axios.get("http://localhost:5000/auth/isAuthorized", {
-				headers: {
-					Authorization: `Bearer ${token}`,
-				}
-			}).then(res => {
-				console.log(res);
-			})
-			.catch(err => {
-			history.push("/login")
-			})
-		else
-			history.push("/login")
-	}
+	// const authHandler = async () => {
+	// 	const token = Cookies.get("access_token")
+	// 	if (token)
+	// 		await axios.get("http://localhost:5000/auth/isAuthorized", {
+	// 			headers: {
+	// 				Authorization: `Bearer ${token}`,
+	// 			}
+	// 		}).then(res => {
+	// 			console.log(res);
+	// 		})
+	// 		.catch(err => {
+	// 		history.push("/login")
+	// 		})
+	// 	else
+	// 		history.push("/login")
+	// }
 
-	useEffect(() => {
-		authHandler();
-	}, []);
+	// useEffect(() => {
+	// 	authHandler();
+	// }, []);
 
     return (
         <MainApp>
