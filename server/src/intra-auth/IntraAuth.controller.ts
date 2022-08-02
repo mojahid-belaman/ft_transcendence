e75380797cbb209effca5e91eb458c472406236b
Khaoula Naboussi <knabouss@e3r1p13.1337.ma>
2022-08-02 20:49:54 +0100

    2fa

--- server/src/intra-auth/IntraAuth.controller.ts
+++ server/src/intra-auth/IntraAuth.controller.ts
@@ -14,6 +14,8 @@ export class IntraAuthController {
   @UseGuards(IntraAuthGuard)
   async login(@Req() req, @Res({ passthrough: true }) res) {
     const userExist = await this.intraAuthService.intraLogin(req);
+    if(userExist && userExist.isTwoFactorAuthEnabled)
+      res.redirect(`http://localhost:3000/twoFactorAuth`);
     const accesToken = await this.authService.login(req.user);
     res.cookie('access_token', accesToken);
     // if (!userExist) return res.redirect(`${process.env.FRONT_END_URI}/`);
@@ -21,6 +23,6 @@ export class IntraAuthController {
     //TOFIX
     if(!userExist)
       return res.redirect(`http://localhost:3000/home`);
-    return res.redirect(`http://localhost:3000/twoFactorAuth`)
+    return res.redirect(`http://localhost:3000/settings`)
   }
 }
