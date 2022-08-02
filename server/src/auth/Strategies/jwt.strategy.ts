ea97dc8fca5ff8f02e0fc303784910277c0b762f
AYOUB BOULBAZ <boulbaz147@gmail.com>
2022-08-02 20:47:01 +0100

    Getting the latest changes

--- server/src/auth/Strategies/jwt.strategy.ts
+++ server/src/auth/Strategies/jwt.strategy.ts
@@ -18,6 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     console.log("payload => ", payload);
     return {
       login: payload.login,
+      // 2-way-authentication
     };
   }
 }
