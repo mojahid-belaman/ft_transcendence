f458648e79ba2ca70b2dd82e06435c6c23582be0
AYOUB BOULBAZ <boulbaz147@gmail.com>
2022-08-08 13:26:14 +0100

    Hame history

--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -10,10 +10,30 @@ export class GameService {
   constructor(
     @InjectRepository(Games)
     private readonly gameRepository: Repository<Games>,
-  ) {}
+  ) { }
 
-  async insertGame(data: AddGameDto) : Promise<Games> {
+  async insertGame(data: AddGameDto): Promise<Games> {
     return await this.gameRepository.save(data);
   }
-  
+
+  async getHistory(userId: string) {
+    return await this.gameRepository.query(`
+    SELECT
+      player1.username as "firstPlayer",
+      player1.avatar as "firstAvatar",
+      games."scoreFirst",
+      player2.username as "secondPlayer",
+      player2.avatar as "secondAvatar",
+      games."scoreSecond",
+      games.date
+    FROM users as player1
+    JOIN games as games ON (games."firstPlayer"::text = player1.id::text)
+    JOIN users as player2 ON (games."secondPlayer"::text = player2.id::text)
+    WHERE player1.id::text = '${userId}' OR player2.id::text = '${userId}'
+    `).then(history => {
+      if (history)
+        return history;
+      return [];
+    })
+  }
 }
